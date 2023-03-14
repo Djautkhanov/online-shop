c5eb7a815d3e8af6e079395fde50603d54c309b0
Aslan <asln86@mail.ru>
2023-03-14 16:05:28 +0300

    исправил недочеты

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import { Footer } from "react-bootstrap/lib/Modal";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 import Cart from "./pages/Cart/Cart";
+import ContactPage from "./pages/ContactPage/ContactPage";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className='App'>
+      <Header/>
+
       <Routes>
         <Route path='/cart' element={<Cart />} />
+        <Route path='/contact' element={<ContactPage />} />
+        <Route path='/' element={<Home />} />
       </Routes>
-      <Footer />
+      <Footer/>
     </div>
   );
 }
