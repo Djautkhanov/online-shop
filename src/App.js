33d095ecae5de53852cf59515c1274458f847e37
Yakumov <dgahar.yakumov@gmail.com>
2023-03-15 11:32:18 +0300

    Сверстал верстку для оформления заказа

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Checkout from './pages/СheckoutPage';
 
 function App() {
   return (
      <div className='App'>
+       <Header/>
+       <Checkout/>
       <Routes>
       </Routes>
        <Footer/> 
+
      </div>
        
   );
